e6dc8c208ac6f1fbaa9ac9a4a8a9c8b8ef335350
Deng Jing <30696785+dropwhatbeat@users.noreply.github.com>
2026-04-27 01:06:41 +0800

    vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import AbsurdTruthsGame from '@/components/absurd-truths/AbsurdTruthsGame'
+import { Analytics } from '@vercel/analytics/next';
+
 
 export default function Home() {
-  return <AbsurdTruthsGame />
+  return (
+    <>
+      <AbsurdTruthsGame />
+      <Analytics />
+    </>
+  )
 }
